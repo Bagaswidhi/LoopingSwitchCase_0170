7809363311812aeb13138d0f690748cda2a5a813
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-03-17 15:33:25 +0700

    Menambahkan for pencacah naik dan for pencacah turun

--- contohPerulangan.cpp
+++ contohPerulangan.cpp
@@ -10,4 +10,19 @@ int main(){
     int perulanganWhile = 0;
     int perulanganDo = 0;
 
+    //Perulangan dengan for pencacah naik
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+    //Perulangan dengan for pencacah turun
+    for (n = 5; n > 0; n--)
+    {
+        cout << "nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
 }
