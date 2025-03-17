fcecdea7adfdd20af3f274b8492b4df5caffc0dc
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-03-17 15:37:42 +0700

    Menambahkan perulangan While dengan angka acak

--- contohPerulangan.cpp
+++ contohPerulangan.cpp
@@ -25,4 +25,19 @@ int main(){
     }
     cout << "Nilai n terakhir = " << n << endl;
     cout << endl;
+
+    //Perulangan while dengan angka acak
+    cout << "Perulangan while" << endl;
+    srand(time(0));
+    n = rand() % 10;
+    cout << "Nilai awal n = " << n << endl;
+    while (n < 7)
+    {
+        cout << "nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+
 }
